9d45ebd7607de03b064776d1e5fbddddf4824e03
James Smith <james@gaslight.co>
2014-11-18 20:05:14 -0500

    Feat: Implements basic Board functionality

--- board.rb
+++ board.rb
@@ -1,5 +1,35 @@
 class Board
+  def initialize(rows=10, cols=rows)
+    @rows = rows
+    @cols = cols
+    @grid = blank_board
+    @brood = " "
+  end
+
   def size
-    100
+    @rows * @cols
+  end
+
+  def draw
+    result = ""
+    @rows.times do |row|
+      @cols.times do |col|
+        result << self[col, row]
+      end
+      result << "\n"
+    end
+    result
+  end
+
+  def []=(x, y, brood)
+    @grid[y][x] = brood
+  end
+
+  def [](x,y)
+    @grid[y][x]
+  end
+
+  private def blank_board
+    Array.new(@rows) { Array.new(@cols) {" "} }
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,4 +3,24 @@ require "spec_helper"
 describe Board do
   Given(:board) { Board.new }
   Then { board.size == 100 }
+  Then { board.draw == Array.new(10) {' '*10}.join("\n")+"\n" }
+  context "specified board" do
+    Given(:board) { Board.new(rows, cols) }
+    context "small board" do
+      Given(:rows) { 4 }
+      Given(:cols) { 6 }
+      Then { board.size == 24 }
+      Then { board.draw == "#{' '*cols}\n"*rows }
+    end
+    context "one brood" do
+      Given(:rows) { 4 }
+      Given(:cols) { 4 }
+      Given(:brood) { 'o' }
+      Given(:x) { 2 }
+      Given(:y) { 3 }
+      When { board[x, y] = brood }
+      Then { board[x, y] == brood }
+      Then { board.draw == [ ' '*cols,' '*cols,' '*cols, '  o ' ].join("\n")+"\n" }
+    end
+  end
 end
